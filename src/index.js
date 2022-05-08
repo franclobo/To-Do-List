0fb7d47e1ef78019ec630f8b6f98f0f89e4ba601
franclobo <fjbl2788@gmail.com>
2022-05-07 23:08:07 -0500

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
-import "./style.css";
-const formList = document.querySelector(".form");
-const addNew = document.querySelector(".input");
-const container = document.querySelector(".list");
-const clearAll = document.querySelector(".clear");
-const upDate = document.querySelector(".refresh");
+import './style.css';
+
+const formList = document.querySelector('.form');
+const addNew = document.querySelector('.input');
+const container = document.querySelector('.list');
+const clearAll = document.querySelector('.clear');
+const upDate = document.querySelector('.refresh');
 
 const updateLocalStorage = () => {
-  const newLists = document.querySelectorAll("li");
+  const newLists = document.querySelectorAll('li');
   list = [];
   newLists.forEach((newList) => {
     list.push({
       name: newList.innerText,
-      completed: newList.classList.contains("checked"),
+      completed: newList.classList.contains('checked'),
     });
   });
   localStorage.setItem("list", JSON.stringify(list));
-}
+};
 
 const toDoList = (task) => {
   let newTask = addNew.value;
