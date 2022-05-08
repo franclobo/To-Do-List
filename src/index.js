60eaafdc6fadd925f91574a0921317fd82198ed9
franclobo <fjbl2788@gmail.com>
2022-05-07 23:03:25 -0500

    Add styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,64 @@
-import _ from 'lodash';
 import "./style.css";
 const formList = document.querySelector(".form");
 const addNew = document.querySelector(".input");
 const container = document.querySelector(".list");
 const clearAll = document.querySelector(".clear");
 const upDate = document.querySelector(".refresh");
+
+const updateLocalStorage = () => {
+  const newLists = document.querySelectorAll("li");
+  list = [];
+  newLists.forEach((newList) => {
+    list.push({
+      name: newList.innerText,
+      completed: newList.classList.contains("checked"),
+    });
+  });
+  localStorage.setItem("list", JSON.stringify(list));
+}
+
+const toDoList = (task) => {
+  let newTask = addNew.value;
+  if (newTask.length < 1) return;
+  if (task) {
+    newTask = task.name;
+  }
+
+  const newList = document.createElement("li");
+  if (task && task.completed) {
+    newList.classList.add("checked");
+  }
+  newList.innerText = newTask;
+  addNew.value = "";
+  const checkBtnEl = document.createElement("div");
+  checkBtnEl.innerHTML = `<i class="fas fa-check-square">`;
+  newList.appendChild(checkBtnEl);
+  container.appendChild(newList);
+  const trashBtnEl = document.createElement("div");
+  trashBtnEl.innerHTML = `<i class="fas fa-trash"></i>`;
+  newList.appendChild(trashBtnEl);
+
+  checkBtnEl.addEventListener("click", () => {
+    newList.classList.toggle("checked");
+    updateLocalStorage();
+  });
+
+  trashBtnEl.addEventListener("click", () => {
+    newList.remove();
+    updateLocalStorage();
+  });
+
+  clearAll.addEventListener("click", () => {
+    if (newList.classList.contains("checked")) {
+      newList.remove();
+      updateLocalStorage();
+    }
+  });
+
+  upDate.addEventListener("click", updateLocalStorage());
+
+  updateLocalStorage();
+}
 
 let list = JSON.parse(localStorage.getItem("list"));
 if (list) {
@@ -17,61 +71,3 @@ formList.addEventListener("submit", (event) => {
   event.preventDefault();
   toDoList();
 });
-
-toDoList = (task) => {
-  let newTask = addNew.value;
-  if (task) {
-    newTask = task.name;
-  }
-
-  const liEl = document.createElement("li");
-  if (task && task.completed) {
-    liEl.classList.add("checked");
-  }
-  liEl.innerText = newTask;
-  container.appendChild(liEl);
-  addNew.value = "";
-  const checkBtnEl = document.createElement("div");
-  checkBtnEl.innerHTML = `
-  <i class="fas fa-check-square">
-  `;
-  liEl.appendChild(checkBtnEl);
-  const trashBtnEl = document.createElement("div");
-  trashBtnEl.innerHTML = `
-  <i class="fas fa-trash"></i>
-  `;
-  liEl.appendChild(trashBtnEl);
-
-  checkBtnEl.addEventListener("click", () => {
-    liEl.classList.toggle("checked");
-    updateLocalStorage();
-  });
-
-  trashBtnEl.addEventListener("click", () => {
-    liEl.remove();
-    updateLocalStorage();
-  });
-
-  clearAll.addEventListener("click", () => {
-    if (liEl.classList.contains("checked")) {
-      liEl.remove();
-      updateLocalStorage();
-    }
-  });
-
-  upDate.addEventListener("click", updateLocalStorage());
-
-  updateLocalStorage();
-}
-
-updateLocalStorage = () => {
-  const liEls = document.querySelectorAll("li");
-  list = [];
-  liEls.forEach((liEl) => {
-    list.push({
-      name: liEl.innerText,
-      completed: liEl.classList.contains("checked"),
-    });
-  });
-  localStorage.setItem("list", JSON.stringify(list));
-}
